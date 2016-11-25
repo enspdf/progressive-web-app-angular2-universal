51d41bb74449cb1cdf0a53407423057a8b79cc79
Sebastian Higuita Castañeda <sebastianhc1995@hotmail.com>
2016-11-24 21:31:56 -0500

    Get all endpoint

--- src/backend/api.ts
+++ src/backend/api.ts
@@ -4,6 +4,12 @@ import { Logro } from './models/logro';
 const router = express.Router();
 
 router.route("/logros")
+  .get((req, res) => {
+    Logro.find({}).exec()
+      .then((documents) => {
+        res.json(documents);
+      });
+  })
   .post((req, res) => {
     const logro = new Logro({
       title: req.body.title,
