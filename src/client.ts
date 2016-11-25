0c6c3d3c6a4947786e4bc2027ccda7c36bddca42
Sebastian Higuita Castañeda <sebastianhc1995@hotmail.com>
2016-11-24 20:53:44 -0500

    Pwa

---
+++ src/client.ts
@@ -0,0 +1,40 @@
+// the polyfills must be the first thing imported
+import 'angular2-universal-polyfills';
+import 'ts-helpers';
+import './__workaround.browser'; // temporary until 2.1.1 things are patched in Core
+
+// Angular 2
+import { enableProdMode } from '@angular/core';
+import { platformUniversalDynamic } from 'angular2-universal/browser';
+
+import { load as loadWebFont } from 'webfontloader';
+// Load fonts async
+// https://github.com/typekit/webfontloader#configuration
+loadWebFont({
+  google: {
+    families: ['Droid Sans']
+  }
+});
+
+// enable prod for faster renders
+// enableProdMode();
+
+import { MainModule } from './browser.module';
+
+export const platformRef = platformUniversalDynamic();
+
+// on document ready bootstrap Angular 2
+export function main() {
+  return platformRef.bootstrapModule(MainModule);
+}
+
+// support async tag or hmr
+switch (document.readyState) {
+  case 'interactive':
+  case 'complete':
+    main();
+    break;
+  case 'loading':
+  default:
+    document.addEventListener('DOMContentLoaded', () => main());
+}
